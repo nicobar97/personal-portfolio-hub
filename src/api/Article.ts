6e3c7d33d98308223823fbd63b9b4c2dff282041
Nicolò Bartelucci <nicobar97@gmail.com>
2023-07-16 00:07:14 +0200

    simplearticle

--- src/api/Article.ts
+++ src/api/Article.ts
@@ -5,18 +5,18 @@ import { Article, SimpleArticle } from '../model/Article';
 import { FetchAuthMapError } from '../model/errors';
 
 export type GetArticles = () => EitherAsync<FetchAuthMapError, SimpleArticle[]>;
-export type GetArticle = () => EitherAsync<FetchAuthMapError, Article>;
+export type GetArticle = (id: string) => EitherAsync<FetchAuthMapError, Article>;
 
 export const getArticles: GetArticles = () =>
   mapFetchFactory().fetch<SimpleArticle[], SimpleArticle[]>(
-    `https://nicobar-portfolio-backend-2a94882f04a5.herokuapp.com/api/article/get`,
+    `https://nicobar-portfolio-backend-2a94882f04a5.herokuapp.com/api/articles/simples`,
     { method: 'GET' },
     (articles) => Right(mapArticles(articles)),
   );
 
-export const getArticle: GetArticle = () =>
+export const getArticle: GetArticle = (id: string) =>
   mapFetchFactory().fetch<Article, Article>(
-    `https://nicobar-portfolio-backend-2a94882f04a5.herokuapp.com/api/article/get`,
+    `https://nicobar-portfolio-backend-2a94882f04a5.herokuapp.com/api/articles/get/${id}`,
     { method: 'GET' },
     (articles) => Right(mapArticle(articles)),
   );
